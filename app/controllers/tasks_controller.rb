a6fa46d3b4fc16a4621c810db2f26319b3e1b916
Josephine Bromley <bromleyjosephine@gmail.com>
2021-01-23 15:40:36 +0000

    added Update functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,30 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(all_task_params)
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def all_task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get "task/:id", to: "tasks#show", as: :task
   get "tasks/new", to: "tasks#new", as: :new
   post "tasks", to: "tasks#create", as: :create
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "task/:id", to: "tasks#update"
 end
