a2ec0feef4204a6ea7519f7e2e26087f7f8cfac7
Josephine Bromley <bromleyjosephine@gmail.com>
2021-01-26 18:33:08 +0000

    tried to add Delete functionality but there is an issue with JS

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
   get "task/:id", to: "tasks#show", as: :task
   get "tasks/new", to: "tasks#new", as: :new
-  post "tasks", to: "tasks#create", as: :create
+  post "tasks", to: "tasks#create"
   get "tasks/:id/edit", to: "tasks#edit", as: :edit
   patch "task/:id", to: "tasks#update"
+  delete "task/:id", to: "tasks#destroy"
 end
